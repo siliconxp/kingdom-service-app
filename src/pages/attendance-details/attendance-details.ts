e22e944f2320d1502b2237d4c71b9c55505b8cf1
Samson Muchindika <kapampale@gmail.com>
2018-05-17 16:31:23 +0200

    attendance

--- src/pages/attendance-details/attendance-details.ts
+++ src/pages/attendance-details/attendance-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 /**
  * Generated class for the AttendanceDetailsPage page.
@@ -15,11 +16,73 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AttendanceDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  attendanceForm: FormGroup;
+  exampleForm: FormGroup;
+
+  items: any = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder) {
+    this.attendanceForm = this.fb.group({
+      customerName: '',
+      email: '',
+      items: this.fb.array([this.createItem()])
+    });
+
+    // expan our form, create form array this._fb.array
+    this.exampleForm = this.fb.group({
+      month: ['', [Validators.required,
+      Validators.maxLength(25)]],      
+      attendances: this.fb.array([
+        this.getUnit()
+      ])
+    });
+  }
+
+  createItem(): FormGroup {
+    return this.fb.group({
+      name: '',
+      description: '',
+      price: ''
+    });
+  }
+
+  addItem(): void {
+    var items = this.attendanceForm.get('items') as FormArray;
+    items.push(this.createItem());
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AttendanceDetailsPage');
+
+
   }
+
+
+
+  // now we create some service methods for create, add and remove 
+  // row inside form
+  // create form row.
+  private getUnit() {
+    const numberPatern = '^[0-9.,]+$';
+    return this.fb.group({
+      //weekName: ['', Validators.required],
+      midweek: ['', [Validators.pattern(numberPatern)]],
+      weekend: ['', [Validators.pattern(numberPatern)]],      
+    });
+  }
+
+  // add new row
+  private addUnit() {
+    const control = <FormArray>this.exampleForm.controls['attendances'];
+    control.push(this.getUnit());
+  }
+
+  // remove row
+  private removeUnit(i: number) {
+    const control = <FormArray>this.exampleForm.controls['attendances'];
+    control.removeAt(i);
+  }
+
+
 
 }
