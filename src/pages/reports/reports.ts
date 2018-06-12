1c2325c078bda968f07194a044d434bb083199ab
Samson Muchindika <kapampale@gmail.com>
2018-06-13 00:18:29 +0200

    9.5

--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { DataServiceProvider } from '../../providers/data-service';
 
+import { from } from 'rxjs/observable/from';
+import { groupBy, mergeMap, toArray } from 'rxjs/operators';
+
 
 /**
  * Generated class for the ReportsPage page.
@@ -18,13 +21,44 @@ import { DataServiceProvider } from '../../providers/data-service';
 })
 export class ReportsPage {
 
-  memberReports:any
+  memberReports: any
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private db:DataServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private db: DataServiceProvider) {
 
     this.db.reports.subscribe(
-      r=>{
-        this.memberReports=this.db.clone(r)
+      r => {
+        //this.memberReports = this.db.clone(r)
+
+        /*    this.memberReports = r.reduce((groups, item) => {
+             const val = item['memberId']
+             groups[val] = groups[val] || []
+             groups[val].push(item)
+             return groups
+   
+           }, {}) */
+
+
+        let group = []
+        r.forEach(
+          rr => {
+
+            var i = group.findIndex((o => o.memberId === rr.memberId));
+            if (i >= 0) {
+              group[i].reports = group[i].reports || []
+              group[i].reports.push(rr)
+            } else {
+              let g = { 'memberId': rr.memberId, 'fname': rr.fname, 'lname': rr.lname, reports: [] }
+              g.reports.push(rr)
+              group.push(g)
+            }
+          }
+        )
+
+        this.memberReports=this.db.clone(group)
+
+
+
+
       }
     );
 
